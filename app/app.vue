072a37be68cc02b8f054ffbeaac78482f68ac1ea
bon3head <justinf0829@gmail.com>
2026-03-03 17:31:17 -0500

    Set up Vue

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="display: flex; justify-content: center; align-items: center; height: 100vh; font-size: 10rem; background-color: black;">
+    🧠 🔜🔜🔜 🌊
+  </div>
+</template>
